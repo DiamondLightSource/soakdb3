5fc363868cc693746f819e2a1b9709ed3437c97c
David Erb <david.erb@diamond.ac.uk>
2023-02-05 07:57:21 +0000

    removes bat file

--- src/soakdb3_xls/soakDB3.bat
+++
@@ -1,2 +0,0 @@
-set SOAKDB3_VISITID=%cd%\..
-start excel /x /r "\\cs04r-nas02-05.diamond.ac.uk\dls_sw\apps\apps\xchem\conda\envs\soakdb3\0.0.5\lib\python3.10\site-packages\soakdb3_xls\soakDB_v3.0.xlsm"
