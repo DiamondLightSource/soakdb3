2c44931d633563785e21e7736627618eeb51df2e
David Erb <david.erb@diamond.ac.uk>
2023-05-26 10:51:26 +0100

    splits up testing

--- src/soakdb3_api/databases/database_definition.py
+++ src/soakdb3_api/databases/database_definition.py
@@ -1,5 +1,7 @@
 import logging
 
+from soakdb3_api.databases.constants import Tablenames
+
 # Base class for all aiosqlite database objects.
 from soakdb3_api.databases.table_definitions import BodyTable, HeadTable, VisitTable
 
@@ -35,27 +37,8 @@ class DatabaseDefinition:
     # ----------------------------------------------------------------------------------------
     async def apply_revision(self, database, revision):
 
-        # Let the base class add any common updates.
-        # Usually only does anything if upgrading to revision 1
-        # which means we are starting with a legacy database with no revision information.
-        await NormsqlAiosqlite.apply_revision(self, database, revision)
-
         # Updating to revision 1 presumably means
         # this is a legacy database with no revision table in it.
+        # TODO: Test revision 1 adding Visit table.
         if revision == 1:
             await database.create_table(Tablenames.VISIT)
-
-        # if revision == 2:
-        #     await self.execute(
-        #         f"ALTER TABLE {Tablenames.ROCKMAKER_IMAGES} ADD COLUMN {ImageFieldnames.NEWFIELD} TEXT",
-        #         why=f"revision 2: add {Tablenames.ROCKMAKER_IMAGES} {ImageFieldnames.NEWFIELD} column",
-        #     )
-        #     await self.execute(
-        #         "CREATE INDEX %s_%s ON %s(%s)"
-        #         % (
-        #             Tablenames.ROCKMAKER_IMAGES,
-        #             ImageFieldnames.NEWFIELD,
-        #             Tablenames.ROCKMAKER_IMAGES,
-        #             ImageFieldnames.NEWFIELD,
-        #         )
-        #     )
--- tests/test_database.py
+++ tests/test_database.py
@@ -1,13 +1,11 @@
-import asyncio
 import logging
-import multiprocessing
 
-import pytest
 from dls_normsql.constants import ClassTypes
 from dls_normsql.databases import Databases
 
 from soakdb3_api.databases.constants import BodyFieldnames, HeadFieldnames, Tablenames
 from soakdb3_api.databases.database_definition import DatabaseDefinition
+from tests.base_tester import BaseTester2
 
 logger = logging.getLogger(__name__)
 
@@ -53,57 +51,7 @@ class TestDatabaseBody:
 
 
 # ----------------------------------------------------------------------------------------
-class TestDatabaseSqliteBackupRestore:
-    def test(self, constants, logging_setup, output_directory):
-        """
-        Tests the sqlite implementation of XchemBeDatabase.
-        """
-
-        database_specification = {
-            "type": "dls_normsql.aiosqlite",
-            "filename": "%s/soakdb3.sqlite" % (output_directory),
-        }
-
-        # Test direct SQL access to the database.
-        DatabaseTesterBackupRestore().main(
-            constants,
-            database_specification,
-            output_directory,
-        )
-
-
-# ----------------------------------------------------------------------------------------
-class _BaseTester:
-    """
-    Provide asyncio loop and error checking over *Tester classes.
-    """
-
-    def main(self, constants, specification, output_directory):
-        """
-        This is the main program which calls the test using asyncio.
-        """
-
-        multiprocessing.current_process().name = "main"
-
-        failure_message = None
-        try:
-            # Run main test in asyncio event loop.
-            asyncio.run(
-                self._main_coroutine(constants, specification, output_directory)
-            )
-
-        except Exception as exception:
-            logger.exception(
-                "unexpected exception in the test method", exc_info=exception
-            )
-            failure_message = str(exception)
-
-        if failure_message is not None:
-            pytest.fail(failure_message)
-
-
-# ----------------------------------------------------------------------------------------
-class DatabaseTesterHead(_BaseTester):
+class DatabaseTesterHead(BaseTester2):
     """
     Test direct SQL access to the database.
     """
@@ -138,7 +86,7 @@ class DatabaseTesterHead(_BaseTester):
 
 
 # ----------------------------------------------------------------------------------------
-class DatabaseTesterBody(_BaseTester):
+class DatabaseTesterBody(BaseTester2):
     """
     Test direct SQL access to the database.
     """
@@ -198,69 +146,6 @@ class DatabaseTesterBody(_BaseTester):
             u2_sql = f"SELECT * FROM {Tablenames.BODY} WHERE {BodyFieldnames.LabVisit} = 'u2' ORDER BY ID ASC"
             records = await database.query(u2_sql)
             assert len(records) == 2, "%s count" % u2_sql
-
-        finally:
-            # Connect from the database... necessary to allow asyncio loop to exit.
-            await database.disconnect()
-
-
-# ----------------------------------------------------------------------------------------
-class DatabaseTesterBackupRestore(_BaseTester):
-    """
-    Test direct SQL backup and restore.
-    """
-
-    async def _main_coroutine(
-        self, constants, database_specification, output_directory
-    ):
-        """ """
-
-        databases = Databases()
-        database = databases.build_object(
-            database_specification,
-            DatabaseDefinition(),
-        )
-
-        # Connect to database.
-        await database.connect(should_drop_database=True)
-
-        try:
-            uuid1 = 1000
-            uuid2 = 2000
-
-            # Write one record.
-            await database.insert(
-                Tablenames.BODY,
-                [{BodyFieldnames.LabVisit: "x", BodyFieldnames.ID: uuid1}],
-            )
-
-            # Backup.
-            await database.backup()
-
-            # Write another record.
-            await database.insert(
-                Tablenames.BODY,
-                [{BodyFieldnames.LabVisit: "y", BodyFieldnames.ID: uuid2}],
-            )
-
-            # Backup again (with two records)
-            await database.backup()
-
-            # Restore one in the past (when it had a single record).
-            await database.restore(1)
-
-            all_sql = (
-                f"SELECT * FROM {Tablenames.BODY} ORDER BY ID ASC /* first query */"
-            )
-            records = await database.query(all_sql)
-            assert len(records) == 1, "first %s count expected 1" % (all_sql)
-
-            # Restore most recent (two records).
-            await database.restore(0)
-
-            all_sql = f"SELECT * FROM {Tablenames.BODY} ORDER BY ID ASC"
-            records = await database.query(all_sql)
-            assert len(records) == 2, "second %s count expected 2" % (all_sql)
 
         finally:
             # Connect from the database... necessary to allow asyncio loop to exit.
